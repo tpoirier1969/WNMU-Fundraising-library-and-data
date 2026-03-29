2d74beaab05a9813445b53ca40b8d04a09150c05
Tod <tpoirier@nmu.edu>
2026-03-29 09:54:25 -0400

    Update app version to v0.14.4

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PLEDGE_MANAGER_CONFIG = {
   APP_NAME: 'WNMU Pledge Program Library',
-  APP_VERSION: 'v0.13.0',
+  APP_VERSION: 'v0.14.4',
   SUPABASE_URL: 'https://tdepltlnughyfrjqufdg.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_oz-1MPs6ix3grIJ7dCbOZg_jYpw6_Q1',
   ADMIN_EMAILS: [],
