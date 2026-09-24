f943faa9d3532e8de173127c0c8ccd150360a10f
Tod <tpoirier@nmu.edu>
2026-09-24 14:53:49 -0400

    Load half-hour strategy worker

--- assets/js/programming-strategy-report.js
+++ assets/js/programming-strategy-report.js
@@ -236,7 +236,7 @@ function runStrategyWorker(schedule){
   return new Promise((resolve,reject)=>{
     let worker;
     try{
-      worker=new Worker('assets/js/programming-strategy-worker.js?v=0.22.197');
+      worker=new Worker('assets/js/programming-strategy-worker.js?v=0.22.198');
     }catch(error){
       reject(error);
       return;
