de858691f149984b912b73806f4e2150dde39e4c
Tod <tpoirier@nmu.edu>
2026-09-25 15:31:31 -0400

    Refresh strategy worker cache

--- assets/js/programming-strategy-report.js
+++ assets/js/programming-strategy-report.js
@@ -224,7 +224,7 @@ function runStrategyWorker(schedule){
   return new Promise((resolve,reject)=>{
     let worker;
     try{
-      worker=new Worker('assets/js/programming-strategy-worker.js?v=0.22.211');
+      worker=new Worker('assets/js/programming-strategy-worker.js?v=0.22.212');
     }catch(error){
       reject(error);
       return;
